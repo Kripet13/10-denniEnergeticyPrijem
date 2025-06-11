6eed662be41e78a629cb8f896cc815b5ba1b10f0
Kripet13 <kristyna.petrinova@student.spsezatec.cz>
2025-06-11 16:19:57 +0200

    Přidána základní struktura HTML a JavaScript

---
+++ script.js
@@ -0,0 +1,24 @@
+let foodList = [
+  { name: "Pizza", kCal: 260 },
+  { name: "Burger", kCal: 295 },
+  { name: "Hranolky", kCal: 312 },
+  { name: "Donut", kCal: 452 },
+  { name: "Smažený sýr", kCal: 380 },
+  { name: "Klobása", kCal: 330 },
+  { name: "KFC kuře (smažené)", kCal: 290 },
+  { name: "Čokoládová tyčinka", kCal: 520 },
+  { name: "Limonáda (slazená)", kCal: 42 },
+  { name: "Zmrzlina", kCal: 210 },
+  { name: "Avokádo", kCal: 160 },
+  { name: "Kuřecí prsa (vařená)", kCal: 165 },
+  { name: "Losos", kCal: 200 },
+  { name: "Brokolice (vařená)", kCal: 35 },
+  { name: "Vejce (vařené)", kCal: 130 },
+  { name: "Mrkev", kCal: 41 },
+  { name: "Jablko", kCal: 52 },
+  { name: "Cizrna (vařená)", kCal: 164 },
+  { name: "Ovesné vločky", kCal: 370 },
+  { name: "Mandle", kCal: 575 }
+];
+
+
